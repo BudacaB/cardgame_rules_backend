e4553615c78f5548074cfc2b0937c52a59737e4d
BudacaB <budaca.b@gmail.com>
2018-12-30 18:56:19 +0200

    GameRulesController and CompareResponse model

---
+++ Controllers/GameRulesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cardgame_rules_backend.Models;
+using cardgame_rules_backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace cardgame_rules_backend
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameRulesController : Controller
+    {
+        [HttpPost]
+        public ActionResult<CompareResult> CompareCards([FromBody] Card[] cards)
+        {
+            if (cards.Length != 2)
+            {
+                var response = new CompareResult();
+                response.Error = "Need two cards";
+                return response;
+            }
+
+            if (cards[0].Number == cards[1].Number)
+            {
+                var response = new CompareResult();
+                response.Error = "Need two different cards";
+                return response;
+            }
+            return WarRulesService.CompareCards(cards[0], cards[1]);
+
+        }
+    }
+}
---
+++ Services/WarRulesService.cs
@@ -0,0 +1,33 @@
+﻿using cardgame_rules_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cardgame_rules_backend.Services
+{
+    public class WarRulesService
+    {
+        
+
+        public static CompareResult CompareCards(Card firstCard, Card secondCard)
+        {
+            //"suit": "hearts",
+            //"number": "2",
+            //"id": "2_of_hearts"
+
+            //if (firstCard.Value < secondCard.Value)
+            //{
+            //    return -1;
+            //}
+            //else if (firstCard.Value == secondCard.Value)
+            //{
+            //    return 0;
+            //}
+            //return 1;
+            var response = new CompareResult();
+            response.Reason = "test";
+            return response;
+        }
+    }
+}
